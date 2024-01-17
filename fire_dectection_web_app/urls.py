bfeb54549132b49997ccd972e44684b8f5f4182c
Minh Đăng Huỳnh <vndang00@gmail.com>
2024-01-17 18:30:14 +0700

    sua file urls.py trong folder fire_dectection_web_app

--- fire_dectection_web_app/urls.py
+++ fire_dectection_web_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name = "dashboard"),
-
+    path('login/', views.LoginPage, name = "login"),
+    path('logout/',views.LogoutPage,name='logout'),
 ]
